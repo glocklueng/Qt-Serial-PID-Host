f2c745962cca96ebae6e0961ab18e2abd3323897
xuafei <xyf0711@foxmail.com>
2015-07-16 18:43:29 +0800

    new

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -9,7 +9,11 @@ MainWindow::MainWindow(QWidget *parent) :
 {
     ui->setupUi(this);
     setupPlot(ui->customPlot);
+
+    isMyComOpen = false;
+    isStart = false;
     ui->button_close->setEnabled(false);
+
     read_saved_pid_parameter();
     select_serial_port();
 }
@@ -21,7 +25,7 @@ void MainWindow::select_serial_port()
     ui->comboBox->removeItem(0);
     ui->comboBox->removeItem(1);
     myCom = new Win_QextSerialPort("COM1",QextSerialBase::EventDriven);
-    for(int i=0; i<10; i++){
+    for(int i=0; i<12; i++){
         portName = QString("COM%0").arg(i+1);
          myCom->setPortName(portName);
          if(myCom->open(QIODevice::ReadWrite) == true){
@@ -30,6 +34,9 @@ void MainWindow::select_serial_port()
          }
     }
     delete myCom;
+    if(ui->comboBox->count() > 0){
+        ui->button_open->setEnabled(true);
+    }
 }
 
 MainWindow::~MainWindow()
@@ -40,27 +47,32 @@ MainWindow::~MainWindow()
 //绘图初始化
 void MainWindow::setupPlot(QCustomPlot *customPlot)
 {
-    x = new QVector<double>;
-    y = new QVector<double>;
-    xt = new QVector<double>;
-    yt = new QVector<double>;
+    t = new QVector<double>;
+    x1 = new QVector<double>;
+    x2 = new QVector<double>;
+    pwm = new QVector<double>;
 
     customPlot->legend->setVisible(true);
     customPlot->legend->setFont(QFont("Helvetica",9));
 
     customPlot->addGraph();
-    customPlot->graph(0)->setName("tg");
-    customPlot->graph(0)->setData(*x, *y);
+    customPlot->graph(0)->setName("x1");
+    customPlot->graph(0)->setData(*t, *x1);
     customPlot->graph(0)->setPen(QPen(Qt::blue));
 
     customPlot->addGraph();
-    customPlot->graph(1)->setName("fb");
-    customPlot->graph(1)->setData(*x, *y);
+    customPlot->graph(1)->setName("x2");
+    customPlot->graph(1)->setData(*t, *x2);
     customPlot->graph(1)->setPen(QPen(Qt::red));
+
+    customPlot->addGraph();
+    customPlot->graph(2)->setName("pwm");
+    customPlot->graph(2)->setData(*t, *pwm);
+    customPlot->graph(2)->setPen(QPen(Qt::green));
 
     customPlot->xAxis->setLabel("time/s");
     customPlot->xAxis->setAutoTickCount(9);
-    customPlot->yAxis->setLabel("speed/(m/s)");
+    customPlot->yAxis->setLabel("angle/(m/s)");
     customPlot->yAxis->setAutoTickCount(9);
     customPlot->xAxis2->setVisible(true);
     customPlot->xAxis2->setTickLabels(false);
@@ -73,26 +85,27 @@ void MainWindow::setupPlot(QCustomPlot *customPlot)
 }
 
 //绘图更新
-void MainWindow::plotUpdate(QCustomPlot *customPlot, double target, double feedback)
+void MainWindow::plotUpdate(QCustomPlot *customPlot, double _x1, double _x2, double _pwm)
 {
     static int num = 0;
     static int counter = 0;
     double time = (counter++)*0.01;
-    x->append(time);
-    y->append(target);
-    xt->append(time);
-    yt->append(feedback);
-    if(counter >= 1200){
-        x->remove(0);
-        y->remove(0);
-        xt->remove(0);
-        yt->remove(0);
+    t->append(time);
+    x1->append(_x1);
+    x2->append(_x2);
+    pwm->append(_pwm);
+    if(counter >= 300){
+        t->remove(0);
+        x1->remove(0);
+        x2->remove(0);
+        pwm->remove(0);
     }
     //customPlot->graph(0)->rescaleAxes(false);
-    customPlot->yAxis->setRange(-0.2, 0.7);
-    customPlot->xAxis->setRange(time-3, time+2);
-    customPlot->graph(0)->setData(*x, *y);
-    customPlot->graph(1)->setData(*xt, *yt);
+    customPlot->yAxis->setRange(-180, 180);
+    customPlot->xAxis->setRange(time-3, time+1);
+    customPlot->graph(0)->setData(*t, *x1);
+    customPlot->graph(1)->setData(*t, *x2);
+    customPlot->graph(2)->setData(*t, *pwm);
     num++;
     if(num == 10){
         num = 0;
@@ -117,12 +130,13 @@ void MainWindow::on_button_open_clicked()
     myCom->setFlowControl(FLOW_OFF);
     myCom->setTimeout(1000);
     connect(myCom,SIGNAL(readyRead()),this,SLOT(readMyCom()));
+    isMyComOpen = true;
 
     ui->button_close->setEnabled(true);
     ui->button_open->setEnabled(false);
 }
 
-//检测可用串口
+//检测按钮回调
 void MainWindow::on_button_filter_clicked()
 {
     select_serial_port();
@@ -130,14 +144,14 @@ void MainWindow::on_button_filter_clicked()
 
 #define BUFFER_HEAD1    0X55
 #define BUFFER_HEAD2    0X66
-#define BUFFER_SIZE     10
+#define BUFFER_SIZE     14
 //读串口
 void MainWindow::readMyCom()
 {
-    static char buffer[BUFFER_SIZE] = {0};
-    static char counter = 0;
+    static unsigned char buffer[BUFFER_SIZE] = {0};
+    static unsigned  char counter = 0;
     char ch;
-    double target, feedback;
+    double _x1, _x2, _pwm;
     while(myCom->bytesAvailable()){
         myCom->getChar(&ch);
         buffer[counter] = ch;
@@ -151,9 +165,10 @@ void MainWindow::readMyCom()
         }
         if(counter == BUFFER_SIZE){
             counter = 0;
-            target = (double)((buffer[2]<<24)|(buffer[3]<<16)|(buffer[4]<<8)|buffer[5])/1000.0f;
-            feedback = (double)((buffer[6]<<24)|(buffer[7]<<16)|(buffer[8]<<8)|buffer[9])/1000.0f;
-            plotUpdate(ui->customPlot, target, feedback);
+            _x1 = (double)((buffer[2]<<24)|(buffer[3]<<16)|(buffer[4]<<8)|buffer[5])/1000.0f-180;
+            _x2 = (double)((buffer[6]<<24)|(buffer[7]<<16)|(buffer[8]<<8)|buffer[9])/1000.0f-180;
+            _pwm = (double)((buffer[10]<<24)|(buffer[11]<<16)|(buffer[12]<<8)|buffer[13])/1000.0f-100;
+            plotUpdate(ui->customPlot, _x1, _x2, _pwm);
         }
     }
 }
@@ -161,6 +176,7 @@ void MainWindow::readMyCom()
 //关闭串口
 void MainWindow::on_button_close_clicked()
 {
+    isMyComOpen = false;
     if(myCom->isOpen()){
         myCom->close();
     }
@@ -173,10 +189,12 @@ void MainWindow::on_button_close_clicked()
 #define HEAD2_A 0X11
 #define HEAD2_B 0X22
 #define HEAD2_C 0X33
+#define HEAD2_SWITCH_START  0XBB
+#define HEAD2_SWITCH_STOP   0XCC
 //发送一个参数
-void MainWindow::send_parameter(float val, char head2)
+void MainWindow::send_parameter(float val, unsigned char head2)
 {
-    char buffer[6] = {0};
+    char buffer[7] = {0};
     buffer[0] = HEAD1;
     buffer[1] = head2;
     unsigned int int_val = (unsigned int)(val*1000+0.5f);
@@ -184,10 +202,9 @@ void MainWindow::send_parameter(float val, char head2)
     buffer[3] = int_val>>16;
     buffer[4] = int_val>>8;
     buffer[5] = int_val;
-    if(myCom != NULL){
-        if(myCom->isOpen()){
-            myCom->write(buffer, 6);
-        }
+    buffer[6] = buffer[2]^buffer[3]^buffer[4]^buffer[5];
+    if(isMyComOpen){
+        myCom->write(buffer, 7);
     }
 }
 
@@ -196,7 +213,7 @@ void MainWindow::on_hSlider_pid_a_valueChanged(int value)
 {
     int min = ui->lineEdit_min_a->text().toInt();
     int max = ui->lineEdit_max_a->text().toInt();
-    float a = value/1000.0f*(max-min)+min;
+    float a = value/100.0f*(max-min)+min;
     QString a_str = QString("%1").arg(a);
     ui->lineEdit_pid_a->setText(a_str);
     send_parameter(a, HEAD2_A);
@@ -207,7 +224,7 @@ void MainWindow::on_hSlider_pid_b_valueChanged(int value)
 {
     int min = ui->lineEdit_min_b->text().toInt();
     int max = ui->lineEdit_max_b->text().toInt();
-    float b = value/1000.0f*(max-min)+min;
+    float b = value/100.0f*(max-min)+min;
     QString b_str = QString("%1").arg(b);
     ui->lineEdit_pid_b->setText(b_str);
     send_parameter(b, HEAD2_B);
@@ -218,7 +235,7 @@ void MainWindow::on_hSlider_pid_c_valueChanged(int value)
 {
     int min = ui->lineEdit_min_c->text().toInt();
     int max = ui->lineEdit_max_c->text().toInt();
-    float c = value/1000.0f*(max-min)+min;
+    float c = value/100.0f*(max-min)+min;
     QString c_str = QString("%1").arg(c);
     ui->lineEdit_pid_c->setText(c_str);
     send_parameter(c, HEAD2_C);
@@ -317,5 +334,14 @@ void MainWindow::closeEvent(QCloseEvent *event)
     save_pid_parameter();
 }
 
-
-
+void MainWindow::on_button_switch_clicked()
+{
+    if(isStart){
+        isStart = false;
+        send_parameter(0, HEAD2_SWITCH_STOP);
+    }
+    else{
+        isStart = true;
+        send_parameter(0, HEAD2_SWITCH_START);
+    }
+}
--- mainwindow.h
+++ mainwindow.h
@@ -22,25 +22,25 @@ private slots:
     void on_button_open_clicked();
     void on_button_filter_clicked();
     void readMyCom();
-
     void on_button_close_clicked();
-
     void on_hSlider_pid_a_valueChanged(int value);
-
     void on_hSlider_pid_b_valueChanged(int value);
-
     void on_hSlider_pid_c_valueChanged(int value);
+
+    void on_button_switch_clicked();
 
 private:
     Ui::MainWindow *ui;
     Win_QextSerialPort *myCom = NULL;
-    QVector<double> *x, *y;
-    QVector<double> *xt, *yt;
+    bool isMyComOpen;
+    bool isStart;
+    QVector<double> *t;
+    QVector<double> *x1, *x2, *pwm;
 
     void setupPlot(QCustomPlot *customPlot);
-    void plotUpdate(QCustomPlot *customPlot, double target, double feedback);
+    void plotUpdate(QCustomPlot *customPlot, double _x1, double _x2, double _pwm);
     void select_serial_port();
-    void send_parameter(float val, char head2);
+    void send_parameter(float val, unsigned char head2);
     void read_saved_pid_parameter();
     void save_pid_parameter();
     void closeEvent(QCloseEvent *event);
